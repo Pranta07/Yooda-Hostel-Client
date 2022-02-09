385c080863a87498d0ea19d4aa6963925693bf69
Pranta07 <palpranta@gmail.com>
2022-02-09 22:44:38 +0600

    loading state removed

--- src/Pages/AddStudent/AddStudent/AddStudent.js
+++ src/Pages/AddStudent/AddStudent/AddStudent.js
@@ -5,7 +5,6 @@ import StudentsTable from "../StudentsTable/StudentsTable";
 
 const AddStudent = () => {
     const [students, setStudents] = useState([]);
-    const [loading, setLoading] = useState(true);
     const [isAdded, setIsAdded] = useState(false);
     const [isUpdated, setIsUpdated] = useState(false);
     const [isDeleted, setIsDeleted] = useState(false);
@@ -15,7 +14,6 @@ const AddStudent = () => {
     const { register, handleSubmit, reset } = useForm();
 
     const onSubmit = (data) => {
-        console.log(data);
         setIsAdded(false);
         fetch("http://localhost:5000/addStudent", {
             method: "POST",
@@ -37,11 +35,9 @@ const AddStudent = () => {
     };
 
     useEffect(() => {
-        setLoading(true);
         fetch(`http://localhost:5000/students?page=${pageNum}`)
             .then((res) => res.json())
             .then((data) => {
-                setLoading(false);
                 setPageCount(Math.ceil(data.count / 5));
                 setStudents(data.students);
             });
@@ -178,70 +174,63 @@ const AddStudent = () => {
                 <h1 className="uppercase tracking-widest text-3xl my-8 pb-2 border-b-4 border-gray-900">
                     Enrolled Students
                 </h1>
-                {loading ? (
-                    <svg
-                        className="my-5 animate-spin h-5 w-5 bg-pink-500 mx-auto"
-                        viewBox="0 0 24 24"
-                    ></svg>
-                ) : (
-                    <>
-                        <StudentsTable
-                            students={students}
-                            setIsUpdated={setIsUpdated}
-                            setIsDeleted={setIsDeleted}
-                        ></StudentsTable>
-                        {/* pagination */}
-                        <div className="mt-4 mb-10 flex justify-center">
-                            <nav
-                                className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
-                                aria-label="Pagination"
+                <>
+                    <StudentsTable
+                        students={students}
+                        setIsUpdated={setIsUpdated}
+                        setIsDeleted={setIsDeleted}
+                    ></StudentsTable>
+                    {/* pagination */}
+                    <div className="mt-4 mb-10 flex justify-center">
+                        <nav
+                            className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+                            aria-label="Pagination"
+                        >
+                            <button
+                                onClick={() => {
+                                    pageNum > 0 && setPageNum(pageNum - 1);
+                                }}
+                                className="relative inline-flex items-center p-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-black hover:text-white hover:bg-pink-500"
                             >
-                                <button
-                                    onClick={() => {
-                                        pageNum > 0 && setPageNum(pageNum - 1);
-                                    }}
-                                    className="relative inline-flex items-center p-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-black hover:text-white hover:bg-pink-500"
-                                >
-                                    <span className="sr-only">Previous</span>
-                                    <ChevronLeftIcon
-                                        className="h-5 w-5"
-                                        aria-hidden="true"
-                                    />
-                                </button>
+                                <span className="sr-only">Previous</span>
+                                <ChevronLeftIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                />
+                            </button>
 
-                                {[...Array(pageCount).keys()].map((number) => (
-                                    <button
-                                        key={number}
-                                        onClick={() => {
-                                            setPageNum(number);
-                                        }}
-                                        className={
-                                            pageNum === number
-                                                ? "bg-pink-500 border-gray-300 text-white relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-                                                : "bg-white border-gray-300 text-black hover:text-white hover:bg-pink-500 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-                                        }
-                                    >
-                                        {number}
-                                    </button>
-                                ))}
-
+                            {[...Array(pageCount).keys()].map((number) => (
                                 <button
+                                    key={number}
                                     onClick={() => {
-                                        pageNum < pageCount - 1 &&
-                                            setPageNum(pageNum + 1);
+                                        setPageNum(number);
                                     }}
-                                    className="relative inline-flex items-center p-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-black hover:text-white hover:bg-pink-500"
+                                    className={
+                                        pageNum === number
+                                            ? "bg-pink-500 border-gray-300 text-white relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                                            : "bg-white border-gray-300 text-black hover:text-white hover:bg-pink-500 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                                    }
                                 >
-                                    <span className="sr-only">Next</span>
-                                    <ChevronRightIcon
-                                        className="h-5 w-5"
-                                        aria-hidden="true"
-                                    />
+                                    {number}
                                 </button>
-                            </nav>
-                        </div>
-                    </>
-                )}
+                            ))}
+
+                            <button
+                                onClick={() => {
+                                    pageNum < pageCount - 1 &&
+                                        setPageNum(pageNum + 1);
+                                }}
+                                className="relative inline-flex items-center p-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-black hover:text-white hover:bg-pink-500"
+                            >
+                                <span className="sr-only">Next</span>
+                                <ChevronRightIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                />
+                            </button>
+                        </nav>
+                    </div>
+                </>
             </div>
         </>
     );
