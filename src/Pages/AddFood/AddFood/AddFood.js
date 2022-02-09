2788d4aa40fe5c12938c7bfdf377fd73d468299d
Pranta07 <palpranta@gmail.com>
2022-02-09 14:21:57 +0600

    pagination done

--- src/Pages/AddFood/AddFood/AddFood.js
+++ src/Pages/AddFood/AddFood/AddFood.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { ExclamationCircleIcon } from "@heroicons/react/outline";
+import {
+    ChevronLeftIcon,
+    ChevronRightIcon,
+    ExclamationCircleIcon,
+} from "@heroicons/react/outline";
 import FoodItemTable from "../FoodItemTable/FoodItemTable";
 
 const AddFood = () => {
     const [foodItems, setFoodItems] = useState([]);
     const [loading, setLoading] = useState(true);
     const [isAdded, setIsAdded] = useState(false);
+    const [pageNum, setPageNum] = useState(0);
+    const [pageCount, setPageCount] = useState(0);
 
     const {
         register,
@@ -39,13 +45,14 @@ const AddFood = () => {
 
     useEffect(() => {
         setLoading(true);
-        fetch(`http://localhost:5000/foods`)
+        fetch(`http://localhost:5000/foods?page=${pageNum}`)
             .then((res) => res.json())
             .then((data) => {
                 setLoading(false);
+                setPageCount(Math.ceil(data.count / 5));
                 setFoodItems(data.foods);
             });
-    }, [isAdded]);
+    }, [isAdded, pageNum]);
 
     return (
         <div className="container mx-auto text-left grid grid-cols-1 lg:grid-cols-2 gap-4">
@@ -102,6 +109,7 @@ const AddFood = () => {
                 </div>
             </div>
 
+            {/* Food Item Table */}
             <div className="px-8">
                 <h1 className="uppercase tracking-widest text-3xl my-8 pb-2 border-b-4 border-gray-900">
                     Available Food Items
@@ -112,7 +120,59 @@ const AddFood = () => {
                         viewBox="0 0 24 24"
                     ></svg>
                 ) : (
-                    <FoodItemTable foods={foodItems}></FoodItemTable>
+                    <>
+                        <FoodItemTable foods={foodItems}></FoodItemTable>
+                        {/* pagination */}
+                        <div className="my-2 flex justify-center">
+                            <nav
+                                className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+                                aria-label="Pagination"
+                            >
+                                <button
+                                    onClick={() => {
+                                        pageNum > 0 && setPageNum(pageNum - 1);
+                                    }}
+                                    className="relative inline-flex items-center p-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-black hover:text-white hover:bg-pink-500"
+                                >
+                                    <span className="sr-only">Previous</span>
+                                    <ChevronLeftIcon
+                                        className="h-5 w-5"
+                                        aria-hidden="true"
+                                    />
+                                </button>
+
+                                {[...Array(pageCount).keys()].map((number) => (
+                                    <button
+                                        key={number}
+                                        onClick={() => {
+                                            setPageNum(number);
+                                        }}
+                                        className={
+                                            pageNum === number
+                                                ? "bg-pink-500 border-gray-300 text-white relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                                                : "bg-white border-gray-300 text-black hover:text-white hover:bg-pink-500 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                                        }
+                                    >
+                                        {number}
+                                    </button>
+                                ))}
+
+                                <button
+                                    onClick={() => {
+                                        pageNum < pageCount - 1 &&
+                                            setPageNum(pageNum + 1);
+                                    }}
+                                    className="relative inline-flex items-center p-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-black hover:text-white hover:bg-pink-500"
+                                >
+                                    <span className="sr-only">Next</span>
+                                    <ChevronRightIcon
+                                        className="h-5 w-5"
+                                        aria-hidden="true"
+                                    />
+                                </button>
+                            </nav>
+                        </div>
+                    </>
                 )}
             </div>
         </div>
